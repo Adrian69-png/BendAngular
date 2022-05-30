4c40d50afd1d82b8bca02560cbf36727d47f9bcc
Olarita Adrian <olaritaa34@gmail.com>
2022-05-31 01:31:11 +0300

    preRefactoring commit

--- src/app/components/new-things/new-things.component.ts
+++ src/app/components/new-things/new-things.component.ts
@@ -1,4 +1,8 @@
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import new_things from "./new_things.json";
+import zones from './zones.json';
+import { ZonesComponent } from 'D:/Angular/Bend/src/app/components/zones/zones.component';
 
 @Component({
   selector: 'app-new-things',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-things.component.css']
 })
 export class NewThingsComponent implements OnInit {
+  
 
-  constructor() { }
-
+  newThings:{id:number, areaId:number, joinedWith:any, sku:string, defaultSku:string, status:string, countActive:number}[] = new_things;
+  zonesData:{areaId:number, name:string}[] = zones;
+  
+  
+  
+  constructor() {
+    
+   }
+  
   ngOnInit(): void {
+    
+        
+     
+   }
   }
 
-}
+
--- src/app/components/zones/zones.component.ts
+++ src/app/components/zones/zones.component.ts
@@ -1,19 +1,113 @@
 import { Component, OnInit } from '@angular/core';
+import new_things from "./new_things.json";
 import zones from './zones.json';
+import { NewThingsComponent } from 'D:/Angular/Bend/src/app/components/new-things/new-things.component';
+
 @Component({
   selector: 'app-zones',
   templateUrl: './zones.component.html',
   styleUrls: ['./zones.component.css']
 })
-export class ZonesComponent implements OnInit {
+export class ZonesComponent  implements OnInit {
 
 
+  newThings:{id:number, areaId:number, joinedWith:any, sku:string, defaultSku:string, status:string, countActive:number}[] = new_things;
   zonesData:{areaId:number, name:string}[] = zones;
-  
-  
-  constructor() {
-    console.log();
 
+  
+  display(zone:number){
+    
+    const zones = this.newThings.map(({ areaId }) => areaId);
+    var display_things:{id:number, areaId:number, joinedWith:any, sku:string, defaultSku:string, status:string, countActive:number}[] = [];
+
+    var list:any= [];
+    var pos = 0;
+    for (var i in zones){
+     if(zones[i] == zone){
+       list[pos] = zones[i];
+       
+       pos++;
+
+     }
+     pos = 0;
+     
+    }
+    for (var i in this.newThings){
+      if(this.newThings[i].areaId == zone){
+          display_things[pos] = this.newThings[i];
+          pos++;
+      }
+    }
+
+    var null_zones:any = [];
+    for (var i in display_things){
+      if (display_things[i].joinedWith == null){
+        null_zones.push(display_things[i]);
+      
+      }
+    }
+    // console.log(null_zones);
+    var sorted_zones:any = [];
+    for (var i in null_zones){
+
+      sorted_zones.push(null_zones[i]);
+
+      for(var j in display_things){
+        if(display_things[j].joinedWith == null_zones[i].id){
+            sorted_zones.push(display_things[j]);
+        } 
+      }
+    }
+
+    
+    return display_things; 
+    
+  }
+
+  get_null_zones(list:any[]){
+    var null_zones:any = [];
+    for (var i in list){
+      if (list[i].joinedWith == null){
+        null_zones.push(list[i]);
+      
+      }
+    }
+    return null_zones
+  }
+  get_sorted_zones(list:any[], list2:any[]){
+    var sorted_zones:any = [];
+    for (var i in list){
+
+      sorted_zones.push(list[i]);
+
+      for(var j in list2){
+        if(list2[j].joinedWith == list[i].id){
+            sorted_zones.push(list2[j]);
+        } 
+      }
+    }
+
+    
+    return sorted_zones;
+
+  }
+
+  get_chain(value:any, list:any[]){
+    var chain_list:any = [];
+      if(value.joinedWith == null){
+        chain_list.push(value);
+        for (var i in list){
+          if(list[i].joinedWith == value.id){
+            chain_list.push(list[i]);
+          }
+        }
+        
+      }
+      return chain_list;
+  }
+ 
+  constructor() {
+    
    }
 
   ngOnInit(): void {
